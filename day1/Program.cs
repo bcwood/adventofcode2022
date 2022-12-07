212f825149237018505bf660ecc023cb1d6c823e
Brandon Wood <bcwood@gmail.com>
2022-12-06 16:08:15 -0900

    refactored day1 to match day2

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,14 +1,24 @@
-﻿Part1();
-Part2();
+﻿using System.Diagnostics;
+
+Stopwatch stopwatch1 = Stopwatch.StartNew();
+long part1 = Part1();
+stopwatch1.Stop();
+
+Stopwatch stopwatch2 = Stopwatch.StartNew();
+long part2 = Part2();
+stopwatch2.Stop();
+
+Console.WriteLine($"Part 1: {part1} ({stopwatch1.ElapsedMilliseconds}ms)");
+Console.WriteLine($"Part 2: {part2} ({stopwatch2.ElapsedMilliseconds}ms)");
 
 Console.WriteLine("\nPress any key to continue...");
 Console.Read();
 
-void Part1()
+long Part1()
 {
+    long maxSum = 0;
     int sum = 0;
-    int maxSum = 0;
-
+    
     foreach (string line in File.ReadAllLines("input.txt"))
     {
         if (!string.IsNullOrWhiteSpace(line))
@@ -22,14 +32,14 @@ void Part1()
         }
     }
 
-    Console.WriteLine(maxSum);
+    return maxSum;
 }
 
-void Part2()
+long Part2()
 {
-    int sum = 0;
-    List<int> sums = new();
-
+    List<long> sums = new();
+    long sum = 0;
+    
     foreach (string line in File.ReadAllLines("input.txt"))
     {
         if (!string.IsNullOrWhiteSpace(line))
@@ -42,7 +52,6 @@ void Part2()
     }
 
     sums.Sort();
-    int top3 = sums.TakeLast(3).Sum();
 
-    Console.WriteLine(top3);
+    return sums.TakeLast(3).Sum();
 }
